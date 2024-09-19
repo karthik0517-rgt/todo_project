ac3af12767a78f393e8f876219022f6a4b593a96
Karthik <karthik.krishnaswamy@ratnaglobaltech.com>
2024-09-19 12:55:57 +0530

    Fix frontend Linting

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,11 +1,13 @@
 module.exports = {
     transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+      '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['@babel/preset-env', '@babel/preset-react'] }],
     },
     transformIgnorePatterns: [
-      '/node_modules/(?!(axios)/)',
+      '/node_modules/(?!axios)/',
     ],
     moduleNameMapper: {
       '^axios$': require.resolve('axios'),
     },
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   };
